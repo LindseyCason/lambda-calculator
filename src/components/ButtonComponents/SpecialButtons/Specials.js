97a256aa00f616582cac55b2f93402fd239ccb98
Lindsey Cason <lindseyacason@gmail.com>
2019-08-13 23:51:40 -0500

    project done with SOME functionality but no calculations

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -2,6 +2,8 @@ import React from "react";
 
 //import any components needed
 
+import { specials } from "../../../data.js"
+import SpecialButton from "./SpecialButton.js"
 //Import your array data to from the provided data file
 
 const Specials = () => {
@@ -12,6 +14,10 @@ const Specials = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+
+       {specials.map(special => <SpecialButton className="specText" text={special}/>)}
     </div>
   );
 };
+
+export default Specials;
